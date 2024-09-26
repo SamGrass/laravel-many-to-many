a7ad13ae1694af324a75c4eec76be7d700e35e88
Samgrass <samuelegrassetti01@gmail.com>
2024-09-26 15:25:10 +0200

    first commit

---
+++ resources/views/admin/partials/aside.blade.php
@@ -0,0 +1,8 @@
+<aside class="text-bg-dark">
+    <ul>
+        <li><a href="{{ route('admin.home') }}">HOME</a></li>
+        <li><a href="{{ route('admin.projects.index') }}">I miei Progetti</a></li>
+        <li><a href="{{ route('admin.projects.create') }}">Aggiungi project</a></li>
+        <li><a href="{{ route('admin.typeProjects') }}">Progetti per categoria</a></li>
+    </ul>
+</aside>
